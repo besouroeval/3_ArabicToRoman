51b79d5bc9c0525597b69b45c830d74d35b6d253
somename <someemail>
2010-10-26 17:45:29 -0200

    besouro automatic message

--- src/Romans.java
+++ src/Romans.java
@@ -3,6 +3,13 @@ public class Romans {
 
 	public static String toRoman(int i) {
 		String roman = "";
+		
+		if (i>5) {
+			roman += "I";
+			i--;
+		}
+			
+		
 		while(i>0) {
 			roman += "I";
 			i--;
