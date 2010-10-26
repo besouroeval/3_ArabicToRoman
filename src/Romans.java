697fcf47df43dd3794ba0c5de37d4f106a8470cc
somename <someemail>
2010-10-26 17:59:08 -0200

    besouro automatic message

--- src/Romans.java
+++ src/Romans.java
@@ -12,31 +12,6 @@ public class Romans {
 			}
 		}
 		
-		while (i>=10) {
-			roman += add(10);
-			i-=10;
-		}
-
-		while (i>=9) {
-			roman += add(9);
-			i-=9;
-		}
-		
-		while (i>=5) {
-			roman += add(5);
-			i-=5;
-		}
-
-		while (i>=4) {
-			roman += add(4);
-			i-=4;
-		}
-		
-		while (i>=1) {
-			roman += add(1);
-			i--;
-		}
-		
 		return roman;
 	}
 
